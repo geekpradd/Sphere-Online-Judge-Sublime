fda23f3625d3c410635d015f644267e9434478d3
Pradipta Bora <pradd@outlook.com>
2015-04-14 12:20:08 +0530

    Output standard Text in place of JSON

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, redirect
-import json
 from core import get_parsed
 app = Flask(__name__) 
 
 @app.route("/problem/<id>")
 def problem(id):
-	return json.dumps(get_parsed(id))
+	return get_parsed(id)
 
 @app.route('/')
 def index():
